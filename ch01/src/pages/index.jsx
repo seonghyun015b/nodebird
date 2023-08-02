bd084029b6e017e830b550c3093d1f6c9a2d2b4c
seonghyun015b <seonghyun015b@gmail.com>
2023-08-02 14:23:22 +0900

    pages 구분, eslint 설치

---
+++ ch01/src/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppLayout from '../components/AppLayout';
+
+const Home = () => {
+  return (
+    <AppLayout>
+      <div>홈</div>
+    </AppLayout>
+  );
+};
+export default Home;
