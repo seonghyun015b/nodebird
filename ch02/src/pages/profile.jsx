e211380418f6a4c2b65620ec6482af5e8308e891
seonghyun015b <seonghyun015b@gmail.com>
2023-08-03 13:39:04 +0900

    _app.js, Head

--- ch02/src/pages/profile.jsx
+++ ch02/src/pages/profile.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import AppLayout from '../components/AppLayout';
+import Head from 'next/head';
 
 const Profile = () => {
   return (
-    <AppLayout>
-      <div>프로필</div>
-    </AppLayout>
+    <>
+      <Head>
+        <title>내 프로필 | NodeBird</title>
+      </Head>
+      <AppLayout>
+        <div>프로필</div>
+      </AppLayout>
+    </>
   );
 };
 export default Profile;
--- ch02/src/pages/signup.jsx
+++ ch02/src/pages/signup.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import AppLayout from '../components/AppLayout';
+import Head from 'next/head';
 
 const Signup = () => {
   return (
-    <AppLayout>
-      <div>회원가입</div>
-    </AppLayout>
+    <>
+      <Head>
+        <title>회원가입 | NodeBird</title>
+      </Head>
+      <AppLayout>
+        <div>회원가입</div>
+      </AppLayout>
+    </>
   );
 };
 export default Signup;
