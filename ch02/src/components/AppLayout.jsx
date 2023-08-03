663cc5a64acc3178eb610f33c8ddfef4ba48b57b
seonghyun015b <seonghyun015b@gmail.com>
2023-08-03 14:55:13 +0900

    로그인 폼

--- ch02/src/components/AppLayout.jsx
+++ ch02/src/components/AppLayout.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { Col, Input, Menu, Row } from 'antd';
 
+import UserProfile from './UserProfile';
+import LoginForm from './LoginForm';
+
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div>
       <Menu mode='horizontal'>
@@ -22,7 +27,11 @@ const AppLayout = ({ children }) => {
       </Menu>
       <Row gutter={8}>
         <Col xs={24} md={6}>
-          왼쪽
+          {isLoggedIn ? (
+            <UserProfile setIsLoggedIn={setIsLoggedIn} />
+          ) : (
+            <LoginForm setIsLoggedIn={setIsLoggedIn} />
+          )}
         </Col>
         <Col xs={24} md={12}>
           {children}
