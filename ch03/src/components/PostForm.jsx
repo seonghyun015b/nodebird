b69601f9809d12720b6da76177f774d3e2eec836
seonghyun015b <seonghyun015b@gmail.com>
2023-08-04 18:11:06 +0900

    post, PostForm

---
+++ ch03/src/components/PostForm.jsx
@@ -0,0 +1,62 @@
+import React, { useCallback, useRef, useState } from 'react';
+import { Button, Form, Input } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+import { addPost } from '../reducers/post';
+
+const PostForm = () => {
+  const imageInput = useRef();
+  const onClickImageUpLoad = useCallback(() => {
+    imageInput.current.click();
+  }, [imageInput.current]);
+
+  const { imagePaths } = useSelector((state) => state.post);
+  const dispatch = useDispatch();
+
+  const [text, setText] = useState('');
+  const onChangeText = useCallback((e) => {
+    setText(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(() => {
+    dispatch(addPost);
+    setText('');
+  }, []);
+
+  return (
+    <Form
+      style={{ margin: '10px 0 20px' }}
+      encType='multipart/form-data'
+      onFinish={onSubmit}
+    >
+      <Input.TextArea
+        value={text}
+        onChange={onChangeText}
+        maxLength={140}
+        placeholder='어떤 신기한 일이 있었나요?'
+      />
+      <div>
+        <input
+          type='file'
+          style={{ display: 'none' }}
+          multiple
+          ref={imageInput}
+        />
+        <Button onClick={onClickImageUpLoad}>이미지 업로드</Button>
+        <Button type='primary' style={{ float: 'right' }} htmlType='submit'>
+          짹짹
+        </Button>
+      </div>
+      <div>
+        {imagePaths.map((v) => (
+          <div key={v} style={{ display: 'inline-block' }}>
+            <img src={v} style={{ width: '200px' }} alt={v} />
+            <div>
+              <Button>제거</Button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </Form>
+  );
+};
+export default PostForm;
--- ch03/src/pages/index.jsx
+++ ch03/src/pages/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import AppLayout from '../components/AppLayout';
+import PostForm from '../components/PostForm';
+import PostCard from '../components/PostCard';
+import { useSelector } from 'react-redux';
 
 const Home = () => {
+  const { isLoggedIn } = useSelector((state) => state.user);
+
+  const { mainPosts } = useSelector((state) => state.post);
+
   return (
     <AppLayout>
-      <div>홈</div>
+      {isLoggedIn && <PostForm />}
+      {mainPosts.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
     </AppLayout>
   );
 };
