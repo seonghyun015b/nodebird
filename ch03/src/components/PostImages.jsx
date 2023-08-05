26620851e6fb28eaec3c13b8f354ede8654c795c
seonghyun015b <seonghyun015b@gmail.com>
2023-08-05 12:13:36 +0900

    댓글 구현

---
+++ ch03/src/components/PostImages.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PostImages = ({ images }) => {
+  return (
+    <>
+      <div>PostImages</div>
+    </>
+  );
+};
+
+PostImages.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.object),
+};
+
+export default PostImages;
