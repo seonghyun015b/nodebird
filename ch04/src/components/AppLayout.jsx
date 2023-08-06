b572bb4172ab221eab51a94511b451e99ae961bb
seonghyun015b <seonghyun015b@gmail.com>
2023-08-06 11:25:54 +0900

    리덕스 사가

---
+++ ch04/src/components/AppLayout.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import { Col, Input, Menu, Row } from 'antd';
+import styled from 'styled-components';
+
+import UserProfile from './UserProfile';
+import LoginForm from './LoginForm';
+import { useSelector } from 'react-redux';
+
+const SearchInput = styled(Input.Search)`
+  vertical-align: middle;
+`;
+
+const AppLayout = ({ children }) => {
+  const { isLoggedIn } = useSelector((state) => state.user);
+
+  return (
+    <div>
+      <Menu mode='horizontal'>
+        <Menu.Item key='home'>
+          <Link href='/'>노드버드</Link>
+        </Menu.Item>
+        <Menu.Item key='profile'>
+          <Link href='/profile'>프로필</Link>
+        </Menu.Item>
+        <Menu.Item key='search'>
+          <SearchInput enterButton style={{ verticalAlign: 'middle' }} />
+        </Menu.Item>
+        <Menu.Item key='signup'>
+          <Link href='/signup'>회원가입</Link>
+        </Menu.Item>
+      </Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href='https://www.naver.com'
+            target='_blank'
+            rel='noreferrer nooppener'
+          >
+            네이버
+          </a>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+AppLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AppLayout;
---
+++ ch04/src/components/PostCard.jsx
@@ -0,0 +1,105 @@
+import React, { useCallback, useState } from 'react';
+import PostImages from './PostImages';
+import PostCardContent from './PostCardContent';
+import CommentForm from './CommentFrom';
+import { Avatar, Button, Card, Popover, List } from 'antd';
+import {
+  EllipsisOutlined,
+  HeartOutlined,
+  HeartTwoTone,
+  MessageOutlined,
+  RetweetOutlined,
+} from '@ant-design/icons';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+
+const PostCard = ({ post }) => {
+  const [liked, setLiked] = useState(false);
+  const [commentFormOpened, setCommentFormOpened] = useState(false);
+
+  const onToggleLiked = useCallback(() => {
+    setLiked((prev) => !prev);
+  }, []);
+
+  const onToggleComment = useCallback(() => {
+    setCommentFormOpened((prev) => !prev);
+  }, []);
+
+  const id = useSelector((state) => state.user.me?.id);
+
+  return (
+    <div style={{ marginBottom: 20 }}>
+      <Card
+        cover={post.Images[0] && <PostImages images={post.Images} />}
+        actions={[
+          <RetweetOutlined key='retweet' />,
+          liked ? (
+            <HeartTwoTone
+              twoToneColor='#eb2f96'
+              key='heart'
+              onClick={onToggleLiked}
+            />
+          ) : (
+            <HeartOutlined key='heart' onClick={onToggleLiked} />
+          ),
+          <MessageOutlined key='comment' onClick={onToggleComment} />,
+          <Popover
+            key='more'
+            content={
+              <Button.Group>
+                {id && post.User.id === id ? (
+                  <>
+                    <Button>수정</Button>
+                    <Button danger>삭제</Button>
+                  </>
+                ) : (
+                  <Button>신고</Button>
+                )}
+              </Button.Group>
+            }
+          >
+            <EllipsisOutlined />
+          </Popover>,
+        ]}
+      >
+        <Card.Meta
+          avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
+          title={post.User.nickname}
+          description={<PostCardContent postData={post.content} />}
+        />
+      </Card>
+      {commentFormOpened && (
+        <div>
+          <CommentForm post={post} />
+          <List
+            header={`${post.Comments.length}개의 댓글`}
+            itemLayout='horizontal'
+            dataSource={post.Comments}
+            renderItem={(item) => (
+              <List.Item>
+                <List.Item.Meta
+                  title={item.User.nickname}
+                  avatar={<Avatar>{item.User.nickname[0]}</Avatar>}
+                  description={item.content}
+                />
+              </List.Item>
+            )}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+PostCard.propTypes = {
+  post: PropTypes.shape({
+    id: PropTypes.number,
+    User: PropTypes.object,
+    content: PropTypes.string,
+    createdAt: PropTypes.object,
+    Comments: PropTypes.arrayOf(PropTypes.object),
+    Images: PropTypes.arrayOf(PropTypes.object),
+  }).isRequired,
+};
+
+export default PostCard;
