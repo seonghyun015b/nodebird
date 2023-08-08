48fd1257354226cc6ec7a26b6bfbfcde583c56e6
seonghyun015b <seonghyun015b@gmail.com>
2023-08-08 14:31:49 +0900

    라우터 분리

--- ch05/back/app.js
+++ ch05/back/app.js
@@ -1,13 +1,19 @@
-const http = require('http');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-  res.write('<h1>Hello Node</h1>');
-  res.write('<h2>Hello Node</h2>');
-  res.write('<h3>Hello Node</h3>');
-  res.end('<h4>Hello Node END</h4>');
+const postRouter = require('./routes/post');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello express');
 });
 
-server.listen(3065, () => {
-  console.log('서버 실행 중');
+app.get('/api', (req, res) => {
+  res.send('hello api');
+});
+
+app.use('/post', postRouter);
+
+app.listen(3065, () => {
+  console.log('서버 실행중');
 });
