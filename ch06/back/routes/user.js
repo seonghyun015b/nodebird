acc51ffadb07cc9c042426b49fd294648fb9ab51
seonghyun015b <seonghyun015b@gmail.com>
2023-09-22 17:07:32 +0900

    swr

--- ch06/back/routes/user.js
+++ ch06/back/routes/user.js
@@ -1,10 +1,91 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
-const { User, Post, Comment, Image } = require('../models');
-const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const router = express.Router();
 
 const passport = require('passport');
-const router = express.Router();
+const bcrypt = require('bcrypt');
+
+const { Op } = require('sequelize');
+
+const { User, Post, Image, Comment } = require('../models');
+
+// middleware
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+
+// 로그인 POST/user/login
+
+router.post('/login', isNotLoggedIn, (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      console.error(err);
+      return next(err);
+    }
+    if (info) {
+      return res.status(403).send(info.reason);
+    }
+    return req.login(user, async (loginErr) => {
+      if (loginErr) {
+        console.error(loginErr);
+        return next(loginErr);
+      }
+      const fullUserWithoutPassword = await User.findOne({
+        where: { id: req.user.id },
+        attributes: {
+          exclude: ['password'],
+        },
+        include: [
+          {
+            model: Post,
+            attributes: ['id'],
+          },
+          {
+            model: User,
+            as: 'Followings',
+            attributes: ['id'],
+          },
+          {
+            model: User,
+            as: 'Followers',
+            attributes: ['id'],
+          },
+        ],
+      });
+      return res.status(200).json(fullUserWithoutPassword);
+    });
+  })(req, res, next);
+});
+
+// 로그아웃 POST/user/logout
+
+router.post('/logout', isLoggedIn, (req, res) => {
+  req.logout(() => {
+    res.send('ok');
+  });
+});
+
+//회원가입 POST / user
+router.post('/', isNotLoggedIn, async (req, res, next) => {
+  try {
+    const exUser = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (exUser) {
+      return res.status(403).send('이미 사용중인 아이디입니다.');
+    }
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    await User.create({
+      email: req.body.email,
+      nickname: req.body.nickname,
+      password: hashedPassword,
+    });
+    res.status(200).send('ok');
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
 // GET /user 유저 정보 불러오기
 
@@ -37,6 +118,42 @@ router.get('/', async (req, res, next) => {
     } else {
       res.status(200).json(null);
     }
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+// GET / user / followers
+
+router.get('/followers', isLoggedIn, async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { id: req.user.id } });
+    if (!user) {
+      res.status(403).send('존재하지 않는 사용자입니다.');
+    }
+    const followers = await user.getFollowers({
+      limit: parseInt(req.query.limit),
+    });
+    res.status(200).json(followers);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+// GET / user / followings
+
+router.get('/followings', isLoggedIn, async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { id: req.user.id } });
+    if (!user) {
+      res.status(403).send('존재하지 않는 사용자입니다.');
+    }
+    const followings = await user.getFollowings({
+      limit: parseInt(req.query.limit),
+    });
+    res.status(200).json(followings);
   } catch (err) {
     console.error(err);
     next(err);
@@ -86,86 +203,6 @@ router.get('/:userId', async (req, res, next) => {
   }
 });
 
-//POST/user/login 로그인
-
-router.post('/login', isNotLoggedIn, (req, res, next) => {
-  passport.authenticate('local', (err, user, info) => {
-    if (err) {
-      console.error(err);
-      return next(err);
-    }
-
-    if (info) {
-      return res.status(401).send(info.reason);
-    }
-
-    return req.login(user, async (loginErr) => {
-      if (loginErr) {
-        console.error(loginErr);
-        return next(loginErr);
-      }
-
-      const fullUserWithoutPassword = await User.findOne({
-        where: { id: user.id },
-        attributes: {
-          exclude: ['password'],
-        },
-        include: [
-          {
-            model: Post,
-            attributes: ['id'],
-          },
-          {
-            model: User,
-            as: 'Followings',
-            attributes: ['id'],
-          },
-          {
-            model: User,
-            as: 'Followers',
-            attributes: ['id'],
-          },
-        ],
-      });
-
-      return res.status(200).json(fullUserWithoutPassword);
-    });
-  })(req, res, next);
-});
-
-// POST/user/logout 로그아웃
-
-router.post('/logout', isLoggedIn, (req, res) => {
-  req.logout(() => {
-    res.send('ok');
-  });
-});
-
-// POST/user 회원가입
-
-router.post('/', isNotLoggedIn, async (req, res, next) => {
-  try {
-    const exUser = await User.findOne({
-      where: {
-        email: req.body.email,
-      },
-    });
-    if (exUser) {
-      return res.status(403).send('이미 사용 중인 아이디입니다.');
-    }
-    const hashedPassword = await bcrypt.hash(req.body.password, 12);
-    await User.create({
-      email: req.body.email,
-      nickname: req.body.nickname,
-      password: hashedPassword,
-    });
-    res.status(201).send('ok');
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
-});
-
 // PATCH / user / 팔로우
 
 router.patch('/:userId/follow', isLoggedIn, async (req, res, next) => {
@@ -198,7 +235,7 @@ router.delete('/:userId/follow', isLoggedIn, async (req, res, next) => {
   }
 });
 
-// DELETE / user / 팔로워 제거
+// DELETE / user /팔로워 제거
 
 router.delete('/follower/:userId', isLoggedIn, async (req, res, next) => {
   try {
@@ -208,38 +245,6 @@ router.delete('/follower/:userId', isLoggedIn, async (req, res, next) => {
     }
     await user.removeFollowings(req.user.id);
     res.status(200).json({ UserId: parseInt(req.params.userId, 10) });
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
-});
-
-// GET / user / followers 목록
-
-router.get('/followers', isLoggedIn, async (req, res, next) => {
-  try {
-    const user = await User.findOne({ where: { id: req.user.id } });
-    if (!user) {
-      res.status(403).send('존재하지 않는 사용자입니다.');
-    }
-    const followers = await user.getFollowers();
-    res.status(200).json(followers);
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
-});
-
-// GET / user / followings 목록
-
-router.get('/followings', isLoggedIn, async (req, res, next) => {
-  try {
-    const user = await User.findOne({ where: { id: req.user.id } });
-    if (!user) {
-      res.status(403).send('존재하지 않는 사용자입니다.');
-    }
-    const followings = await user.getFollowings();
-    res.status(200).json(followings);
   } catch (err) {
     console.error(err);
     next(err);
--- ch06/front/src/components/FollowList.jsx
+++ ch06/front/src/components/FollowList.jsx
@@ -1,10 +1,10 @@
 import React, { useMemo } from 'react';
-import { Button, Card, List } from 'antd';
-import { StopOutlined } from '@ant-design/icons';
 import PropTypes from 'prop-types';
+import { StopOutlined } from '@ant-design/icons';
+import { Button, List, Card } from 'antd';
 import { styled } from 'styled-components';
+import { UNFOLLOW_REQUEST, REMOVE_FOLLOWER_REQUEST } from '../reducers/user';
 import { useDispatch } from 'react-redux';
-import { REMOVE_FOLLOWER_REQUEST, UNFOLLOW_REQUEST } from '../reducers/user';
 
 const ListStyle = styled(List)`
   margin-bottom: 20px;
@@ -19,8 +19,8 @@ const ListItem = styled(List.Item)`
   margin-top: 20;
 `;
 
-const FollowList = ({ header, data }) => {
-  const listGrid = useMemo(() => ({ gutter: 4, xs: 2, md: 3 }), []);
+const FollowList = ({ header, data, onClickMore, loading }) => {
+  const listGrid = useMemo(() => ({ gutter: 4, xs: 2, md: 3 }, []));
 
   const dispatch = useDispatch();
 
@@ -38,33 +38,41 @@ const FollowList = ({ header, data }) => {
   };
 
   return (
-    <ListStyle
-      grid={listGrid}
-      size='small'
-      header={<div>{header}</div>}
-      loadMore={
-        <ListDiv>
-          <Button>더 보기</Button>
-        </ListDiv>
-      }
-      bordered
-      dataSource={data}
-      renderItem={(item) => (
-        <ListItem>
-          <Card
-            actions={[<StopOutlined key='stop' onClick={onCancel(item.id)} />]}
-          >
-            <Card.Meta description={item.nickname} />
-          </Card>
-        </ListItem>
-      )}
-    />
+    <>
+      <ListStyle
+        grid={listGrid}
+        size='small'
+        header={<div>{header}</div>}
+        loadMore={
+          <ListDiv>
+            <Button onClick={onClickMore} loading={loading}>
+              더 보기
+            </Button>
+          </ListDiv>
+        }
+        bordered
+        dataSource={data}
+        renderItem={(item) => (
+          <ListItem>
+            <Card
+              actions={[
+                <StopOutlined key='stop' onClick={onCancel(item.id)} />,
+              ]}
+            >
+              <Card.Meta description={item.nickname} />
+            </Card>
+          </ListItem>
+        )}
+      />
+    </>
   );
 };
 
 FollowList.propTypes = {
   header: PropTypes.string.isRequired,
   data: PropTypes.array.isRequired,
+  onClickMore: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
 };
 
 export default FollowList;
